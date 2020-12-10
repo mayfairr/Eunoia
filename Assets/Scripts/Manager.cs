63ef37ad8953db7d6707e563d6fadf8afa7c31ec
Zeeshan <14zkhan@rgshw.com>
2020-12-10 19:02:09 +0000

    Genetic Algorithm
    Working iteration of Genetic Algorithm learning method.

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,21 +4,73 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    public int populationCount = 100;
-    public GameObject Origin;
+    public int populationCount;
+    public int generation;
 
-    private void Start()
+    public GameObject target;
+    public GameObject origin;
+
+    private List<NeuralNetwork> neuralNetworks;
+    private List<GameObject> agents;
+
+    public float repeatRate;
+    public float timeScale;
+
+    public int layers;
+    public int[] structure;
+    public void Start()
     {
+        structure = new int[]{ 4,10,15,1};
+        createNeuralNetworks();
+        InvokeRepeating("createAgents", .1f, repeatRate);
+    }
+    public void createNeuralNetworks()
+    {
+        neuralNetworks = new List<NeuralNetwork>();
         for(int i = 0; i < populationCount; i++)
         {
-            //Create GameObject;
+            NeuralNetwork nn = new NeuralNetwork(structure);
+            neuralNetworks.Add(nn);
+        }
+  
+    }
+    public void createAgents()
+    {
+        Time.timeScale = timeScale;
+        if(agents != null)
+        {
+            for(int i = 0; i < populationCount; i++)
+            {
+                Destroy(agents[i]);
+            }
+            sortNetwork();
+            
+        }
+        agents = new List<GameObject>();
+        for (int i = 0; i < populationCount; i++){
             GameObject agent = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
-            //Parent agent script
             agent.AddComponent<Agent>();
-            agent.GetComponent<Agent>().constVelocity = 3f;
-            agent.GetComponent<Agent>().target = null;
-            agent.GetComponent<Agent>().Origin = Origin.transform.position; 
+            agent.GetComponent<Agent>().neuralNetwork = neuralNetworks[i];
+            agent.GetComponent<Agent>().target = target;
+            agent.GetComponent<Agent>().constVelocity = 3.0f;
+            agent.GetComponent<Agent>().Origin = origin.transform.position;
+            agent.layer = LayerMask.NameToLayer("Agent");
+            agent.transform.tag = "Agent";
+            agents.Add(agent);
+            
+        }
+    }
+
+    public void sortNetwork()
+    {
+        //sorts the neural network list in ascending order in terms of fitness, 0 being really bad.
+        this.neuralNetworks.Sort();
+
+        //Make bottom a copied version of top half with some mutations. 
+        for(int i = 0; i < populationCount/2; i++)
+        {
+            neuralNetworks[i] = new NeuralNetwork(neuralNetworks[populationCount / 2 + i]);
+            neuralNetworks[i].mutate();
         }
     }
 }
